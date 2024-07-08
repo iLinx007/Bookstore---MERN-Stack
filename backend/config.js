229f609f89af1ea58f58dca84f3b22eb33d1a09f
iLinx007 <ahiadulincoln@gmail.com>
2024-07-08 23:00:13 +0400

    mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+"mongodb+srv://ahiadulincoln:ahiadulincoln@books-store-mern.3iyqkut.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"

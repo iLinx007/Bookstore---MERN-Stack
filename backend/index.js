548a516419a6979ab553964fee13d4267b8afb93
iLinx007 <ahiadulincoln@gmail.com>
2024-07-20 16:13:49 +0400

    spinner

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose'
 import {Book} from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,11 +11,11 @@ app.use(express.json());
 
 app.use(cors());
 
-// app.use(cors{
+// app.use(cors({
 //     origin: 'http://localhost:5555',
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     allowedHeaders: ['Content-Type'],
-// })
+// }));
 
 app.get('/', (request, response) => {
     console.log(request)
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import {AiOutlineEdit} from 'react-icons/ai';
+import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md';
+import {BsInfoCircle} from 'react-icons/bs';
 
 const Home = () => {
   return (

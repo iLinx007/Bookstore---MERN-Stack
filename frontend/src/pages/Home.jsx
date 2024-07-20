6a31241a3bf4e19f75907f7020c7d485b5ef933a
iLinx007 <ahiadulincoln@gmail.com>
2024-07-20 18:50:09 +0400

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,8 +7,55 @@ import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md';
 import {BsInfoCircle} from 'react-icons/bs';
 
 const Home = () => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+
+        axios
+        .get('http://localhost:5555/books')
+        .then((response) => {
+            setBooks(response.data.data);
+            setLoading(false);
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoading(false);
+        })
+
+    }, []);
   return (
-    <div>Home</div>
+    <div className='p-4'>
+        <div className='flex justify-between items-center'>
+            <h1 className='text-3xl my-8'>
+                Books List
+            </h1>
+            <Link to='/books/create'>
+            <MdOutlineAddBox className='text-sky-800 text-4xl'/>
+            </Link>
+        </div>
+
+        {loading ? (
+            <Spinner/>
+        ) : (
+            <table className='w-full border-separate border-spacing-2'>
+                <thead>
+                    <tr>
+                        <th className='border border-slate-600 rounded-md'>No</th>
+                        <th className='border border-slate-600 rounded-md'>Title</th>
+                        <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
+                        <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+                        <th className='border border-slate-600 rounded-md'>Operations</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        )
+        }
+    </div>
   )
 }
 

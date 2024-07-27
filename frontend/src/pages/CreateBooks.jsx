148d94a65c54fcc2282f5e21396055c3fb261358
iLinx007 <ahiadulincoln@gmail.com>
2024-07-27 12:01:47 +0000

    Create books page

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setpublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    }
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then (() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occured. Please check the console for more info');
+        console.log(error);
+      });
+  }
+
+
   return (
     <div>CreateBooks</div>
   )

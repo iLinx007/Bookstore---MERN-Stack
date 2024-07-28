f1bb13968e551398b6d75bb549c8a2a4c5dfc969
iLinx007 <ahiadulincoln@gmail.com>
2024-07-28 21:03:37 +0000

    delete book page

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams} from 'react-router-dom';
+
 
 const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
   return (
     <div>DeleteBook</div>
   )
